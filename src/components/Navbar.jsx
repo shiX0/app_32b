ca26329e0df29aa826f00eb03adb7699e4a473c2
shishir <shisirsharma22@gmail.com>
2024-05-02 09:15:32 +0545

    added states

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand">MeoW!</a>
-    <form class="d-flex" role="search">
-      <Link to={'/login'} class="btn btn-outline-success me-3" type="submit" >Login</Link>
-      <Link to={'/register'} class="btn btn-outline-danger" type="submit">Register</Link>
+            <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand">MeoW!</a>
+    <form className="d-flex" role="search">
+      <Link to={'/login'} className="btn btn-outline-success me-3" type="submit" >Login</Link>
+      <Link to={'/register'} className="btn btn-outline-danger" type="submit">Register</Link>
     </form>
   </div>
 </nav>        </>
--- src/pages/register/Registerpage.jsx
+++ src/pages/register/Registerpage.jsx
@@ -4,15 +4,65 @@ import{useState} from 'react'
 function Registerpage() {
     // State management
     // format(variable.changing var)=state
-    const [firstName,setFirstname]=useState();
-    const [lasrNamer,setLastname]=useState();
-    const [email,setEmail]=useState();
-    const [password,setPassword]=useState();
+    const [firstName,setFirstname]=useState('');
+    const [lastNamer,setLastname]=useState('');
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+
+  // State for handeling error
+    const [firstNameError,setFirstnameError]=useState();
+    const [lastNamerError,setLastnameError]=useState();
+    const [emailError,setEmailError]=useState();
+    const [passwordError,setPasswordError]=useState();
 
     // MAke a funciton to save the state
     const handelFirstName =(e)=>
     {
       setFirstname(e.target.value)
+    }
+    const handelLastName =(e)=>
+    {
+      setLastname(e.target.value)
+    }
+    const handelEmail =(e)=>
+    {
+      setEmail(e.target.value)
+    }
+    const handelPassword =(e)=>
+    {
+      setPassword(e.target.value)
+    }
+
+    const validate=()=>{
+      var isValid=true;
+      if(firstName.trim()===''){
+        setFirstnameError('please enter first name')
+        isValid=false;
+      }
+      if(lastNamer.trim()===''){
+        setLastnameError('please enter last name')
+        isValid=false;
+      }
+      if(email.trim()===''){
+        setEmailError('please enter email')
+        isValid=false;
+      }
+      if(password.trim()===''){
+        setPasswordError('please enter password')
+        isValid=false;
+      }
+      return isValid;
+    } 
+
+
+    // For button
+    const handelSubmit=(e)=>{
+      e.preventDefault();
+      const isvalid = validate();
+      if(!isvalid){
+        return
+      }
+      // console.log(firstName,lastNamer,email,password);
     }
 
 
@@ -22,15 +72,28 @@ function Registerpage() {
     <h1>Register</h1>
     <div className="d-flex">
       <form action="">
-        <label onChange={handelFirstName} htmlFor="">First Name</label>
-        <input type="text" className='form-control ' placeholder='Enter your First Name'/>
+        <label  htmlFor="">First Name: </label>
+        <input onChange={handelFirstName} type="text" className='form-control ' placeholder='Enter your First Name'/>
+        {
+          firstNameError && <p className='text-danger'>{firstNameError}</p>
+        }
+
         <label className='mt-2' htmlFor="">last Name</label>
-        <input type="text" className='form-control' placeholder='Enter your Last Name'/>
+        <input onChange={handelLastName} type="text" className='form-control' placeholder='Enter your Last Name'/>
+        {
+          lastNamerError && <p className='text-danger'>{lastNamerError}</p>
+        }
         <label className='mt-2' htmlFor="">Email</label>
-        <input type="email" className='form-control' placeholder='Enter your Email'/>
+        <input onChange={handelEmail} type="email" className='form-control' placeholder='Enter your Email'/>
+        {
+          emailError && <p className='text-danger'>{emailError}</p>
+        }
         <label className='mt-2' htmlFor="">Password</label>
-        <input type="password" className='form-control' placeholder='Enter your Password'/>
-        <button className='btn btn-outline-danger'>Register</button>
+        <input onChange={handelPassword} type="text" className='form-control' placeholder='Enter your Password'/>
+        {
+          passwordError && <p className='text-danger'>{passwordError}</p>
+        }
+        <button onClick={handelSubmit} className='btn btn-outline-danger'>Register</button>
         </form>
     </div>  
 
